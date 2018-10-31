685642075abb8733e3b3ea0d400f2e7c4c725070
Pham thanh Tu <tubackkhoa@gmail.com>
2018-10-31 12:09:26 +0700

    update install_requirements file

---
+++ install-requirements.sh
@@ -0,0 +1,5 @@
+go get github.com/emirpasic/gods/trees/redblacktree
+go get github.com/fatih/color
+go get github.com/shopspring/decimal
+go get gopkg.in/oleiade/lane.v1
+go get github.com/manifoldco/promptui
