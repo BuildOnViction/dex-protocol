f21bd5d7ec1852f73c3daaf486916a71e8921b4b
Hoang Trinh <hoang.trinhj@gmail.com>
2018-11-28 22:41:06 +0700

    Add necessary packages

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,8 +1,10 @@
+go get github.com/ethereum/go-ethereum
 go get github.com/emirpasic/gods/trees/redblacktree
 go get github.com/fatih/color
 go get github.com/shopspring/decimal
 go get gopkg.in/oleiade/lane.v1
 go get github.com/manifoldco/promptui
+go get gopkg.in/urfave/cli.v1
 
 # swap 
 go get github.com/tyler-smith/go-bip32
